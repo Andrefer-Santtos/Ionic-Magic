1177c9568e1c44e397c865a442ee1300c948a238
Andrefer-Santtos <1andre.santos.souza1@gmail.com>
2022-02-11 10:08:29 -0300

    +funcionalidades para o carrinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   public appPages = [
     { title: 'Profile', url: 'profile', icon: 'person-circle' },
     { title: 'Categorias', url: 'categorias', icon: 'apps' },
+    { title: 'Carrinho', url: 'cart', icon: 'cart'},
     { title: 'Logout', url: '', icon: 'log-out' }
   ];
   constructor(public authService: AuthService, public navCtrl: NavController) { }
--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 import { CartItem } from 'src/models/cart-item';
+import { ProdutoDTO } from 'src/models/produto.dto';
 import { CartService } from 'src/services/domain/cart.service';
 
 @Component({
@@ -11,11 +13,33 @@ export class CartPage {
 
   items: CartItem[];
 
-  constructor(public cartService: CartService) { }
+  constructor(
+    public cartService: CartService,
+    public navCtrl: NavController) { }
 
   ionViewDidEnter() {
     let cart = this.cartService.getCart();
     this.items = cart.items;
     console.log(this.items)
+  }
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
+  }
+
+  goBuy() {
+    this.navCtrl.navigateRoot('categorias');
   }
 }
